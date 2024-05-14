4a32abf63a586ae0f52e28da8bfa74cba9671a2b
Mário Alexandre Rodrigues <48701214+MrBros@users.noreply.github.com>
2024-05-14 14:57:27 -0300

    Update wallTracker.ino

--- robo_movel/examples/wallTracker/wallTracker.ino
+++ robo_movel/examples/wallTracker/wallTracker.ino
@@ -7,5 +7,17 @@ void setup() {
 
 void loop() {
   wallTracker();
+  
+  Serial.print("RIGHT MOTOR:");
+  Serial.print(getRightMotorRPM());
+  Serial.print(" RPM - ");
+  Serial.print(getRightMotorDistance());
+  Serial.println(" cm");
+
+  Serial.print("LEFT MOTOR:");
+  Serial.print(getLeftMotorRPM());
+  Serial.print(" RPM - ");
+  Serial.print(getLeftMotorDistance());
+  Serial.println(" cm");
 }
 
