c34564ca152690ad00d9d91785777dca8f503c6d
Mário Alexandre Rodrigues <48701214+MrBros@users.noreply.github.com>
2024-05-18 03:39:56 -0300

    Add files via upload

--- robo_movel/examples/rotationTest/rotationTest.ino
+++ robo_movel/examples/rotationTest/rotationTest.ino
@@ -1,1 +1,46 @@
+#include <robo_movel.h>
 
+void setup() {
+  setupConfig(115200);
+  delay(3000);
+}
+
+long time;
+
+void loop() {
+  time = millis();
+  rotate("clockwise","right", 105);
+  Serial.print("Time: ");
+  Serial.println(millis() - time);
+  delay(1000);
+
+  stop();
+  delay(1000);
+
+  time = millis();
+  rotate("clockwise","left", 85);
+  Serial.print("Time: ");
+  Serial.println(millis() - time);
+  delay(1000);
+
+  stop();
+  delay(1000);
+  
+  time = millis();
+  rotate("antiClockwise","right", 360);
+  Serial.print("Time: ");
+  Serial.println(millis() - time);
+  delay(1000);
+
+  stop();
+  delay(1000);
+
+  time = millis();
+  rotate("antiClockwise","left", 360);
+  Serial.print("Time: ");
+  Serial.println(millis() - time);
+  delay(1000);
+
+  stop();
+  delay(2000);
+}
